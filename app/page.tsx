c8cf746b7e7625d720517bdc7165a0434addd79c
lilraz70 <mahamadou.abdoulrazak@yahoo.com>
2024-12-16 10:54:45 +0100

    fin realise 1

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import AboutSection from "@/components/sections/AboutSection"
 import BlogSection from "@/components/sections/BlogSection"
 import ContactSection from "@/components/sections/ContactSection"
 import HeroSection from "@/components/sections/HeroSection"
-import MouseSection from "@/components/sections/MouseSection"
+// import MouseSection from "@/components/sections/MouseSection"
 import ProjectSection from "@/components/sections/ProjectSection"
 
 export default function Home() {
   return (
     <main id="main">
-      <MouseSection />
+      {/* <MouseSection /> */}
       <HeroSection />
       <AboutSection />
       <ProjectSection />
