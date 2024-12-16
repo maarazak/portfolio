cced0348a8aa324e4674836deeb958aeca79ef87
lilraz70 <mahamadou.abdoulrazak@yahoo.com>
2024-12-16 11:00:25 +0100

    fin realise 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 // import AboutSection from "@/components/sections/AboutSection"
 // import BlogSection from "@/components/sections/BlogSection"
 // import ContactSection from "@/components/sections/ContactSection"
-// import HeroSection from "@/components/sections/HeroSection"
-// import MouseSection from "@/components/sections/MouseSection"
+import HeroSection from "@/components/sections/HeroSection"
+import MouseSection from "@/components/sections/MouseSection"
+
 // import ProjectSection from "@/components/sections/ProjectSection"
 
 export default function Home() {
   return (
     <main id="main">
-      {/* <MouseSection /> */}
-      {/* <HeroSection /> */}
+      <MouseSection />
+      <HeroSection />
       {/* <AboutSection /> */}
       {/* <ProjectSection /> */}
       {/* <BlogSection /> */}
